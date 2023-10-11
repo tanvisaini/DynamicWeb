48a7d98b01f07e546e3a474b589e5d3e5e83adb1
Tanvi Saini <ts4175@nyu.edu>
2023-10-11 12:22:47 -0400

    week 6

---
+++ Week_6/image-search/src/App.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+import searchImages from './api'
+import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList'
+
+export default function App() {
+  const [images, setImages] = useState([])
+
+  const handleSubmit = async(term) => {
+    //console.log('do a search with: ', term)
+    const result = await searchImages(term)
+    setImages(result)
+  }
+
+  return (
+    <>
+      
+      <SearchBar onSubmit={handleSubmit}/>
+      <ImageList images={images}/>
+    </>
+  );
+}
+
